863e8d24e745fd0345be67ba9936b9030a8fa62a
Subhani6697 <shaiksubhani6697@gmail.com>
2024-11-19 16:45:06 -0600

    all done

--- routes/bookshelves.js
+++ routes/bookshelves.js
@@ -1,8 +1,8 @@
 var express = require('express');
-const artifact_controller = require('../controllers/bookshelveController');
+const bookshelve_controller = require('../controllers/bookshelve');
 var router = express.Router();
-// GET request to fetch all artifacts
+// GET request to fetch all bookshelve
 router.get('/', bookshelve_controller.bookshelve_list);
-// POST request to create a new artifact
+// POST request to create a new bookshelve
 router.post('/bookshelves', bookshelve_controller.bookshelve_create_post);
 module.exports = router;
