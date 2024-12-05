f853c06b00c79ef871c8b70502fda0075f53561b
Subhani6697 <shaiksubhani6697@gmail.com>
2024-12-04 23:55:15 -0600

    small changes

--- models/bookshelve.js
+++ models/bookshelve.js
@@ -4,14 +4,14 @@ const BookshelveSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Bookshelve name is required'],
-        minlength: [3, 'Bookshelve name must be at least 3 characters long'],  // Minimum length for name
-        maxlength: [100, 'Bookshelve name cannot exceed 100 characters'],  // Maximum length for name
+        minlength: [3, 'Bookshelve name must be at least 3 characters long'],  
+        maxlength: [100, 'Bookshelve name cannot exceed 100 characters'], 
     },
     material: {
         type: Number,
         required: [true, 'Material is required'],
-        min: [1, 'Material must be at least 1'],  // Capacity should be at least 1
-        max: [1000, 'Material seems too large, check for valid input'],  // Upper limit for capacity
+        min: [1, 'Material must be at least 1'],  
+        max: [1000, 'Material seems too large, check for valid input'], 
     },
     shelves: {
         type: String,
